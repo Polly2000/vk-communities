044bdfc767414f4d26e738562e4f66d66b206cc8
Polly2000 <kuznetsovapolina2020@gmail.com>
2024-03-10 11:57:03 +0300

    fix: style for friends

--- src/components/GroupItem/FriendsBlock.tsx
+++ src/components/GroupItem/FriendsBlock.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
-import { Caption } from '@vkontakte/vkui';
+import { Caption, useAppearance } from '@vkontakte/vkui';
 import { User } from '../../redux/groups/types';
 import classes from './GroupItem.module.css';
 
 const FriendsBlock: FC<any> = ({ friends }) => {
+  const appearance = useAppearance();
   const [friendsClicked, setFriendsClicked] = useState<boolean>(false);
   const friendsCount: number | undefined = friends?.length || 0;
 
@@ -17,7 +18,7 @@ const FriendsBlock: FC<any> = ({ friends }) => {
         Друзья: {friendsCount}
       </Caption>
       {friendsClicked === true && (
-        <div className={classes.userFriends}>
+        <div className={appearance === 'light' ? classes.userFriends : classes.userFriendsDark}>
           {friends.map((item: User, index: number) => (
             <Caption level="2" key={index}>{`${item.first_name} ${item.last_name}`}</Caption>
           ))}
